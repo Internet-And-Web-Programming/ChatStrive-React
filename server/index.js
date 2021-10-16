485e0f3e357d086879c1ff696ffa84f42a28d39b
TanmayKumar-EngStud <tanmay.kumar2019@vitstudent.ac.in>
2021-10-16 17:22:25 +0530

    temp

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+const port = 4500 || process.env.PORT;
+const server = http.createServer(app);
+const io = socketIO(server);
+app.use(cors());
+
+io.on("connection", () => {
+  console.log("New user connected");
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port http://localhost:${port}`);
+});
