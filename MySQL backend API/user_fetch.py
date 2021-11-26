f18723fbac9d7baf9b91b50dc2e8d0ee4498b948
AyushGupta2301 <ayush2301ak@gmail.com>
2021-11-26 09:22:08 +0530

    minor changes

--- MySQL backend API/user_fetch.py
+++ MySQL backend API/user_fetch.py
@@ -1,4 +1,4 @@
 import requests
 
-resp = requests.post('http://192.168.1.10:4500/sign_in',json={"UserID":"user2"})
-print(resp.text)
+resp = requests.post('http://127.0.0.1:4500/sign_in',json={"UserID":"user300"})
+print(resp.status_code)
