fd04b6df17755c0439209ab37025ebc8fb3e2587
Ria Arun <81374415+riaarun25@users.noreply.github.com>
2021-11-28 22:13:15 +0530

    Update ContactUs.jsx

--- client/src/components/ContactUs/ContactUs.jsx
+++ client/src/components/ContactUs/ContactUs.jsx
@@ -46,7 +46,17 @@ function ContactUs() {
       {/* <Btn style={btnLocation} /> */}
       <div style={style2}>
         <h1>Contact Us</h1>
-        <p>Sorry But this service is currently unavailable</p>
+        /*<p>Sorry But this service is currently unavailable</p>*/
+        /*Added section*/
+         <form id="contact-form" method="POST">
+          <label htmlFor="name">Full Name</label>
+          <input name="name" placeholder="Enter full name..." type="text" />
+          <label htmlFor="email">Email</label>
+          <input name="email" placeholder="Enter email..." type="email" />
+          <label htmlFor="message">Message</label>
+          <textarea rows="6" placeholder="Enter message..." name="message" required></textarea>
+          <button type="submit"> Send Message</button>
+        </form>
       </div>
     </div>
   );
