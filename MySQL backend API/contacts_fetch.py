ed57e70325e1b7013d51e4e2c415d623431c0b97
TanmayKumar-EngStud <tanmay.kumar2019@vitstudent.ac.in>
2021-11-29 17:57:43 +0530

    removing unual files

--- MySQL backend API/contacts_fetch.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-# returns contacts in the form of a JSON object list in JSON string, used JSON.load to convert to JSON object list
-resp = requests.post('http://192.168.1.10:4500/fetch_contacts',json={'User1':'user5'})
-r = json.loads(resp.text)
-print(r)
--- MySQL backend API/message_fetch.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-resp = requests.post('http://192.168.1.10:4500/fetch_message',json={'senderID':'user4','recvID':'user3'})
-r = json.loads(resp.text)
-print(r)
